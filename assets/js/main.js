7d43a0e291f64077fe84209791ad1e1753940ffc
Oleksii Akastolov <akastolov1@gmail.com>
2022-05-30 23:52:56 +0300

    закончил две части с респонсивом

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,9 @@ $(document).ready(function () {
   });
 
 });
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function(){
+		$(this).toggleClass('header-navbar__btn--open');
+    $('.header-navbar__list').stop(true, true).slideToggle(300);
+	});
+});
